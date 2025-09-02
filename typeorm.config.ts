eb2743ec4a0d657f7711e77a1f77bd9ad5a8d80c
Estime299 <estimegliti108@gmail.com>
2025-09-02 10:50:53 +0100

    add config update date:2/09/25

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -5,15 +5,22 @@ import { CommentModel } from './src/comment/models/comment.model/comment.model';
 import { LikeModel } from './src/like/models/like.model/like.model';
 import { FollowModel } from './src/follow/models/follow.model/follow.model';
 import { GroupModel } from './src/group/models/group.model/group.model';
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
+
+const config = yaml.load(
+  fs.readFileSync('./src/config/default.yml', 'utf8'),
+) as any;
+
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  synchronize: false,
-  logging: true,
+  type: config.database.type,
+  host: config.database.host,
+  port: config.database.port,
+  username: config.database.username,
+  password: config.database.password,
+  database: config.database.database,
+  synchronize: config.database.synchronize,
+  logging: config.database.logging,
   entities: [
     UserModel,
     PostModel,
